a3c948da14b36466e96ce911da148c2a55824465
Melinda Serven <melinda.serven@acquia.com>
2022-03-23 10:30:43 -0700

    re-add module5_code.go

---
+++ module5/module5_code.go
@@ -0,0 +1,14 @@
+package module5
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func GetExampleDotCom() {
+	resp, err := http.Get("http://example.com/")
+	if err != nil {
+		fmt.Println("something went wrong")
+	}
+	defer resp.Body.Close()
+}
