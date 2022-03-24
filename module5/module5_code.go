10f05aa87ddc28964f31d5c56b048f8617da29ac
Melinda Serven <melinda.serven@acquia.com>
2022-03-24 09:20:52 -0700

    copy from the answer branch

--- module5/module5_code.go
+++ module5/module5_code.go
@@ -5,10 +5,12 @@ import (
 	"net/http"
 )
 
+// GetExampleDotCom uses the "net/http" package to send a GET request to example.com
 func GetExampleDotCom() {
 	resp, err := http.Get("http://example.com/")
 	if err != nil {
 		fmt.Println("something went wrong")
 	}
+
 	defer resp.Body.Close()
 }
